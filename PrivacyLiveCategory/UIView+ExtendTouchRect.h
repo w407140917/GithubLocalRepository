1e223839e64157be56d2ecbbb3576165ff24fffd
w407140917 <41049817+w407140917@users.noreply.github.com>
2018-08-07 17:17:42 +0800

    addCatrgory

---
+++ PrivacyLiveCategory/UIView+ExtendTouchRect.h
@@ -0,0 +1,13 @@
+//
+//  UIView+ExtendTouchRect.h
+//  demo
+//
+//  Created by codeBoy on 16/1/1.
+//  Copyright © 2016年 codeBoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ExtendTouchRect)
+@property (nonatomic, assign) UIEdgeInsets touchExtendInset;
+@end
